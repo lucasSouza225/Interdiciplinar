9a06582b0ad630c9c3a2155b6ec642336e479feb
lucas souza <lucas2469y@gmail.com>
2025-06-16 18:13:10 -0300

    aqui eu fiz uns ajustes no menu moble

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-function iniciarMenuResponsivo() {
-  const menuBtn = document.getElementById('menuBtn');
-  const mobileMenu = document.getElementById('mobileMenu');
-
+// funçao responsável por tornar o menu responsivo 
+function btnResponsivo() {
+  // obtém o botão do menu pelo id menuBtn
+  let menuBtn = document.getElementById('menuBtn');
+  // obtém o menu que será exibido ou ocultado no modo mobile
+  let mobileMenu = document.getElementById('mobileMenu');
+  // Verifica se os dois elementos existem na página
   if (menuBtn && mobileMenu) {
+    // Adiciona um ouvinte de evento de clique no botão do menu
     menuBtn.addEventListener('click', () => {
+        
+      // alterna a classe 'active' no menu mobile
+
       mobileMenu.classList.toggle('active');
     });
+          // 'active' isso serve para mostrar ou esconder o menu, dependendo do CSS associado à classe 
   }
 }
 
-// Executa a função ao carregar a página
-document.addEventListener('DOMContentLoaded', iniciarMenuResponsivo);
+// Executa a função btnResponsivo assim que o conteúdo da página for carregado
+document.addEventListener('DOMContentLoaded', btnResponsivo);
